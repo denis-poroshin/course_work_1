cd204f19cae7ddd3620510d00ff052655f2db0e7
original <git@github.com:denis-poroshin/variables.git>
2024-03-01 21:46:47 +0300

    Начало

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+
+    //Employee[] employees = new Employee[10];
+
+    public static void main(String[] args) {
+        Employee[] employees = new Employee[10];
+    }
+
+
+
+
+}
