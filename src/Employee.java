123dfae2027427d21837acc00769b983a3c8fc71
original <git@github.com:denis-poroshin/variables.git>
2024-03-01 21:48:52 +0300

    Курсовая работа выполнена

--- src/Employee.java
+++ src/Employee.java
@@ -2,6 +2,88 @@ import java.util.Objects;
 
 public class Employee {
 
+    private int department; // Отдел
+    private String name; // Имя
+    private String familName; // Фамилия
+    private String surname; // Отчество
+    private int salary; // Зарплата
+    public static int id = 0; // id - счетчик
+    public Employee (int department, String name, String familName, String surname, int salary){
+        this.department = department; // Отдел
+        this.name = name; // Имя
+        this.familName = familName; // Фамилия
+        this.surname = surname; // Отчество
+        this.salary = salary; // Зарплата
+
+
+
+    }
+    public static int sumId(){
+        id++;
+
+        return id;
+    }
+
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFamilName() {
+        return familName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && salary == employee.salary && Objects.equals(name, employee.name) && Objects.equals(familName, employee.familName) && Objects.equals(surname, employee.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(department, name, familName, surname, salary);
+    }
+    public void printAllEmployees(){
+        for (int i = 0; i < id; i++){
+            System.out.println(getName());
+        }
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "\n" + "Номер отдела: " + department + "\n" + "ФИО: " +  familName + " "
+                + name + " " + surname + "\n" + "Зарплата: " + salary;
+    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,112 @@ public class Main {
 
     public static void main(String[] args) {
         Employee[] employees = new Employee[10];
-    }
 
+
+        employees[0] = new Employee(1, "Иванов", "Иван", "Иванович", 60_000);
+        employees[1] = new Employee(2, "Иванов", "Евгений", "Анатольевич", 65_000);
+        employees[2] = new Employee(3, "Иванов", "Степан", "Сергеевич", 70_000);
+        employees[3] = new Employee(4, "Иванов", "Игнат", "Андреевич", 75_000);
+        employees[4] = new Employee(5, "Иванов", "Аркадий", "Семенович", 80_000);
+
+        printAllEmployees(employees); // Получить список всех сотрудников
+        System.out.println();
+
+        System.out.println("Сумма затрат на ЗП в месяц: " + salaryPerMonth(employees)); // Сумма затрат на ЗП в месяц
+        System.out.println();
+        minimumSalaryCalculation(employees); // Сотрудних с менимальной ЗП:
+        System.out.println();
+        maximumSalaryCalculation(employees); // Сотрудних с максимальной ЗП:
+        System.out.println();
+        System.out.println("Средняя заработная плата: " + calculationOfAverageSalary(employees));
+        System.out.println();
+        printFullnamesOfEmployees(employees); // ФИО всех сотрудников
+
+
+
+    }
+    public static void printAllEmployees(Employee[] employees){
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null){
+                break;
+            }
+            System.out.print("id: " + Employee.sumId());
+            System.out.println(employees[i]);
+
+        }
+    }
+    public static int salaryPerMonth(Employee[]employees){
+        int total = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null){
+                break;
+            }
+            total += employees[i].getSalary();
+
+        }
+        return total;
+    }
+    public static void minimumSalaryCalculation(Employee[]employees){
+        int minSalary = salaryPerMonth(employees);
+        int identifier = 0;
+        for (int i = 0; i < employees.length; i++) {
+
+            if (employees[i] == null){
+                break;
+            }
+
+            if (minSalary > employees[i].getSalary()){
+                minSalary = employees[i].getSalary();
+                identifier = i;
+            }
+
+        }
+        System.out.println("Сотрудних с менимальной ЗП: " + employees[identifier]);
+
+    }
+    public static void maximumSalaryCalculation(Employee[]employees){
+        int maxSalary = 0;
+        int identifier = 0;
+        for (int i = 0; i < employees.length; i++) {
+
+            if (employees[i] == null){
+                break;
+            }
+
+            if (maxSalary < employees[i].getSalary()){
+                maxSalary = employees[i].getSalary();
+                identifier = i;
+            }
+
+        }
+        System.out.println("Сотрудних с максимальной ЗП: " + employees[identifier]);
+
+    }
+    public static int calculationOfAverageSalary(Employee[]employees){
+        int monthlyEmployeeSalary = salaryPerMonth(employees);
+        int total = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null){
+                break;
+            }
+            total++;
+
+        }
+        return  (monthlyEmployeeSalary / total);
+
+
+    }
+    public static void printFullnamesOfEmployees(Employee[]employees){
+        System.out.println("ФИО всех сотрудников: ");
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null){
+                break;
+            }
+            System.out.println(employees[i].getFamilName() + " " + employees[i].getName() + " " + employees[i].getSurname());
+
+        }
+
+    }
 
 
 
